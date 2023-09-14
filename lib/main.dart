c4b80cc1a891188ebd7d4d74b857f8fc12cdab89
Ahram Kim <ahram0223@gmail.com>
2023-09-15 00:30:16 +0900

    // 230915 : Introduction Screen/Onboarding Screen UI

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       routes: {
+        // 230915 : Introduction Screen/Onboarding Screen UI
         "/": (context) => OnBoarding(),
         "home": (context) => HomeScreen(),
       },
