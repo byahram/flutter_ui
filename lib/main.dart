01631201a596f580a866b75017a64b216fc51ae3
Ahram Kim <ahram0223@gmail.com>
2023-09-28 02:02:41 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ui/HomeScreen.dart';
+import 'package:flutter_ui/onBoarding/HomeScreen.dart';
 import 'package:flutter_ui/onBoarding/OnBoarding.dart';
 
 void main() {
@@ -13,8 +13,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       routes: {
         // 230915 : Introduction Screen/Onboarding Screen UI
-        "/": (context) => OnBoarding(),
-        "home": (context) => HomeScreen(),
+        "/": (context) => const OnBoarding(),
+        "home": (context) => const HomeScreen(),
       },
     );
   }
